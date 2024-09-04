bcbab7852236d2f7735d71610044d17c54d2c4ac
jeongwoo903 <www8565@naver.com>
2024-09-05 03:08:51 +0900

    feat: 필요 라이브러리 세팅 완료

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+  },
+
+  // swcMinify: 코드 최소화 (Minification)
+  swcMinify: true,
+
+
+  compiler: {
+    // styled-component의 SSR 최적화 & 디스플레이 네임 활성화
+    styledComponents: true,
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  }
+};
 
 export default nextConfig;
