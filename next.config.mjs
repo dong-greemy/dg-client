fd34c2381cc6a3eaaa7ed99ca6d1f54b0d86ea79
jeongwoo903 <www8565@naver.com>
2024-09-08 19:22:19 +0900

    fix: turbopack -> webpack 으로 전환

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-  },
+import withPWAInit from "@ducanh2912/next-pwa";
 
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+const nextConfig = {
   compiler: {
     // styled-component의 SSR 최적화 & 디스플레이 네임 활성화
     // babel-plugin-styled-components 대체
     styledComponents: true,
-  },
-
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
   }
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
