b8699a93181f9513f45d2778e8b1322489d3eeab
jeongwoo903 <www8565@naver.com>
2024-12-03 15:32:09 +0900

    chore: 렌더링 추적을 위한 `react-scan` 설치

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,19 @@
 'use client';
-
 import { PropsWithChildren } from 'react';
+import { scan } from 'react-scan';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalPortal } from 'components/GlobalPortal';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+
+/** 개발용 react-scan 세팅 */
+if (typeof window !== 'undefined') {
+  scan({
+    enabled: true,
+    log: true, // logs render info to console (default: false)
+  });
+}
 
 export function Providers({ children }: PropsWithChildren) {
   return (
