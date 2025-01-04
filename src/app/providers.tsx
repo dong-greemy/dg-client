3b519c72374716a34c2c6b40eeaf3a8060b62ab0
jeongwoo903 <www8565@naver.com>
2025-01-04 14:34:05 +0900

    feat: tanstack-query 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,29 +1,40 @@
 'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
-import { scan } from 'react-scan';
+// import { scan } from 'react-scan';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalPortal } from 'components/GlobalPortal';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
-/** 개발용 react-scan 세팅 */
-if (typeof window !== 'undefined') {
-  scan({
-    enabled: true,
-    log: true, // logs render info to console (default: false)
-  });
-}
+// /** 개발용 react-scan 세팅 */
+// if (typeof window !== 'undefined') {
+//   scan({
+//     enabled: true,
+//     log: true, // logs render info to console (default: false)
+//   });
+// }
 
 export function Providers({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 2,
+      },
+    },
+  });
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalPortal.Provider>
-          <GlobalStyles />
-          {children}
-        </GlobalPortal.Provider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalPortal.Provider>
+            <GlobalStyles />
+            {children}
+          </GlobalPortal.Provider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
