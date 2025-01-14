5107ed0c40e30fec6fa1a9fd548ae704c0380c6d
jeongwoo903 <www8565@naver.com>
2025-01-14 23:53:07 +0900

    refactor: `SearchBar` 컴포넌트 액션 변경 및 비즈니스 로직 분리
    - 기존의 google places api에서 네이버 search api로 전환.
    - 또한 SearchBar의 액션과 비즈니스 로직을 분리하여 범용성 있게 사용하기 위해 수정.

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,37 +1,27 @@
 import { useState } from 'react';
 
-import { PlaceReqType } from 'apis/searchPlace';
 import { useDebounce } from 'hooks/useDebounce';
 import BackIcon from 'svg/back-icon.svg';
 import ClearIcon from 'svg/clear-icon.svg';
+import { PlaceReqType } from 'types/place';
 
 import * as S from './styled';
 
-export const SearchBar = ({
-  searchGoogle,
-  onClear,
-  onTextChange,
-}: {
-  searchGoogle: (searchData: PlaceReqType) => void;
+interface SearchBarProps {
+  searchAction: (searchData: PlaceReqType) => void;
   onClear: () => void;
-  onTextChange: (value: string) => void;
-}) => {
+  placeholder: string;
+}
+
+export const SearchBar = ({ searchAction, onClear, placeholder }: SearchBarProps) => {
   const [text, setText] = useState<string>('');
 
   const debouncedSearch = useDebounce((value: string) => {
-    searchGoogle({ text: value, display: 15 });
+    searchAction({ text: value, display: 15 });
   }, 300);
 
   const handleSearch = (value: string) => {
     setText(value);
-
-    if (!value) {
-      onClear();
-      return;
-    }
-
-    setText(value);
-    onTextChange(value);
     debouncedSearch(value);
   };
 
@@ -43,7 +33,7 @@ export const SearchBar = ({
       <S.SearchInput
         value={text}
         onChange={e => handleSearch(e.target.value)}
-        placeholder={'지하철 역, 장소 검색'}
+        placeholder={placeholder}
       />
       <S.SearchClear
         onClick={() => {
